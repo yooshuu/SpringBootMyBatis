5051efb448f1baafdce505086f4856ddba305152
yooshuu <yooshuu11@gmail.com>
2026-07-02 16:48:14 +0900

    소스 수정

--- src/main/java/kopo/poly/SpringBootMyBatisApplication.java
+++ src/main/java/kopo/poly/SpringBootMyBatisApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootMyBatis2Application {
+public class SpringBootMyBatisApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringBootMyBatis2Application.class, args);
+        SpringApplication.run(SpringBootMyBatisApplication.class, args);
     }
 
 }
--- src/main/java/kopo/poly/service/impl/NoticeService.java
+++ src/main/java/kopo/poly/service/impl/NoticeService.java
@@ -1,4 +1,75 @@
 package kopo.poly.service.impl;
 
-public class NoticeService {
+import kopo.poly.dto.NoticeDTO;
+import kopo.poly.mapper.INoticeMapper;
+import kopo.poly.service.INoticeService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Slf4j
+@RequiredArgsConstructor
+@Service
+public class NoticeService implements INoticeService {
+
+    // RequiredArgsConstructor 어노테이션으로 생성자를 자동 생성함
+    // noticeMapper 변수에 이미 메모리에 올라간 INoticeMapper 객체를 넣어줌
+    // 예전에는 autowired 어노테이션을 통해 설정했었지만, 이젠 생성자를 통해 객체 주입함
+    private final INoticeMapper noticeMapper;
+
+
+    @Override
+    public List<NoticeDTO> getNoticeList() throws Exception {
+
+        log.info("{}.getNoticeList start!", this.getClass().getName());
+
+        return noticeMapper.getNoticeList();
+
+    }
+
+    @Transactional
+    @Override
+    public NoticeDTO getNoticeInfo(NoticeDTO pDTO, boolean type) throws Exception {
+
+        log.info("{}.getNoticeInfo start!", this.getClass().getName());
+
+        if (type) {
+            log.info("Update ReadCNT");
+
+            noticeMapper.updateNoticeReadCnt(pDTO);
+        }
+        return noticeMapper.getNoticeInfo(pDTO);
+    }
+
+    @Transactional
+    @Override
+    public void insertNoticeInfo(NoticeDTO pDTO) throws Exception {
+
+        log.info("{}.InsertNoticeInfo start!", this.getClass().getName());
+
+        noticeMapper.insertNoticeInfo(pDTO);
+    }
+
+    @Transactional
+    @Override
+    public void updateNoticeInfo(NoticeDTO pDTO) throws Exception {
+
+        log.info("{}.updateNoticeInfo start!", this.getClass().getName());
+
+        noticeMapper.updateNoticeInfo(pDTO);
+
+    }
+
+    @Transactional
+    @Override
+    public void deleteNoticeInfo(NoticeDTO pDTO) throws Exception {
+
+        log.info("{}.deleteNoticeInfo start!", this.getClass().getName());
+
+        noticeMapper.deleteNoticeInfo(pDTO);
+
+    }
 }
